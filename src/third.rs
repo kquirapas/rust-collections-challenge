65f5a971088e3c57a3f4875f9464c59847f2799c
Kristian Quirapas <kmquirapas@gmail.com>
2022-06-11 19:54:47 +0800

    Added file open and creation in third.rs

--- src/third.rs
+++ src/third.rs
@@ -1,5 +1,25 @@
+use std::io::{Error, ErrorKind};
+use std::fs::File;
 
+const PATH: &str = "./files/data.csv";
 
 pub fn run() {
     println!("===== THIRD =====");
+
+
+    let f = match File::open(PATH) {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create(PATH) {
+                Ok(file_created) => {
+                    println!("File created: {}", PATH);
+                    file_created
+                },
+                Err(create_error) => panic!("File creation failed: {:?}", create_error)
+            },
+            other_error => {
+                panic!("File open failed: {:?}", other_error);
+            }
+        }
+    };
 }
